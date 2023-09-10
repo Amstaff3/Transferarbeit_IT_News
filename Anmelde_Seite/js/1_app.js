03c208bbc0b6fb3ce7d3cc6aa503a88e43d26be1
Amstaff3 <57978580+Amstaff3@users.noreply.github.com>
2023-09-10 16:08:31 +0200

    Hinzu löschen von Usern

--- Anmelde_Seite/js/1_app.js
+++ Anmelde_Seite/js/1_app.js
@@ -1,4 +1,76 @@
+/*Code hinzu für Kommunikation mit dem Server*/
 document.querySelector('#registracija').addEventListener('click', () => {
+    document.querySelector('.custom-modal').style.display = 'block'; 
+});
+
+document.querySelector('#closeModal').addEventListener('click', () => {
+    document.querySelector('.custom-modal').style.display = 'none'; 
+});
+
+let config = {
+    'korisnicko_ime': {
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    'register_email': {
+        required: true,
+        email: true,
+        minlength: 5,
+        maxlength: 50,
+    },
+    'register_lozinka': {
+        required: true,
+        minlength: 7,
+        maxlength: 25,
+        matching: 'ponovi_lozinku'
+    },
+    'ponovi_lozinku': {
+        required: true,
+        minlength: 7,
+        maxlength: 25,
+        matching: 'register_lozinka'
+    }
+};
+
+let validator = new Validator(config, '#registrationForm');
+
+document.querySelector('#registrationForm').addEventListener('submit', async e => {
+    e.preventDefault();
+
+    if (validator.validationPassed()) {
+        const formData = {
+            korisnicko_ime: document.querySelector('#user_name').value,
+            register_email: document.querySelector('#email').value,
+            register_lozinka: document.querySelector('#lozinka').value,
+        };
+
+        try {
+            const response = await fetch('http://localhost:3000/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+
+            const data = await response.json();
+
+            if (data.message) {
+                alert(data.message);
+            } else {
+                alert('Ein Fehler ist aufgetreten.');
+            }
+        } catch (error) {
+            console.error('Es gab einen Fehler:', error);
+            alert('Ein Fehler ist aufgetreten.');
+        }
+    } else {
+        alert('Nicht richtig ausgefüllt');
+    }
+});
+
+/*document.querySelector('#registracija').addEventListener('click', () => {
     document.querySelector ('.custom-modal').style.display = 'block'; 
 });
 
@@ -47,3 +119,4 @@ document.querySelector('#registrationForm').addEventListener('submit', e => {
         alert('Nicht richtig ausgefüllt');
     }
 });
+*/
--- server.js
+++ server.js
@@ -1,4 +1,73 @@
+/*Hinzu löschen von Usern*/
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.use(express.json());
+
+// Verbinden Sie sich mit MongoDB
+mongoose.connect('mongodb://localhost:27017/userAuth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Mongoose Schema und Modell für Benutzer
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: { type: String, unique: true },
+    password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Endpoint für die Benutzerregistrierung
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username, email, password: hashedPassword });
+        await user.save();
+        res.status(201).send({ message: 'User registered successfully!' });
+    } catch (error) {
+        res.status(400).send({ error: 'Registration failed!' });
+    }
+});
+
+// Endpoint für die Benutzeranmeldung
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).send({ error: 'User not found!' });
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) return res.status(400).send({ error: 'Invalid password!' });
+
+    const token = jwt.sign({ id: user._id }, 'SECRET_KEY', { expiresIn: '1h' });
+    res.send({ message: 'Logged in successfully!', token });
+});
+
+// Endpoint zum Löschen eines Benutzers
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const userId = req.params.id;
+        await User.findByIdAndRemove(userId);
+        res.status(200).send({ message: 'User successfully deleted' });
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to delete user' });
+    }
+});
+
+// Starten Sie den Server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+/*const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -54,3 +123,4 @@ app.post('/login', async (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+*/
